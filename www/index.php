874692705b1e551e767a21cac3de3de20600e155
vladbalach <vladbalachevskyy@gmail.com>
2018-10-20 15:22:21 +0300

    index.php v0.01
    Links on our pages

--- www/index.php
+++ www/index.php
@@ -8,9 +8,9 @@
 	<p align="center"> Главая страница</p>
 
 	<p><a href="vlad/vlad.php">Vlad</a></p>
-	<p><a href="slavik.php">Slavik</a></p>
-	<p><a href="ilya.php">Ilya</a></p>
-	<p><a href="Rostik.php">Rostik</a></p>
+	<p><a href="slavik/slavik.php">Slavik</a></p>
+	<p><a href="ilya/ilya.php">Ilya</a></p>
+	<p><a href="rostik/rostik.php">Rostik</a></p>
 </body>
 
 </html>
