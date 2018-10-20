6dc40d568041f468476dfb7f3fb713b150c69dc7
vladbalach <vladbalachevskyy@gmail.com>
2018-10-20 18:45:13 +0300

    Blocks1

--- vlad/vlad.php
+++ vlad/vlad.php
@@ -2,9 +2,24 @@
 <html>
 <head>
 	<link href="../style.css" rel="stylesheet">
+	<link href="vlad.css" rel="stylesheet">
 	<title>Vlad</title>
 </head>
 <body>
 
+	<div id="V_DivHeader">
+	<p align="center"><font size=72px>Привет, это сайт Влада</font></p>
+	</div>
+	<div id="V_BlockParent">
+		<div id="V_Block1">
+			1 Block
+		</div>
+		<div id="V_Block2">
+			2 Block
+		</div>
+		<div id="V_Block3">
+			3 Block
+		</div>
+	</div>
 </body>
 </html>
