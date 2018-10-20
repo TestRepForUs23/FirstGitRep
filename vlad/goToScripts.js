44479485425f16e2029b2c7ed47398094d2b1a06
vladbalach <vladbalachevskyy@gmail.com>
2018-10-20 22:57:14 +0300

    popup1

--- vlad/goToScripts.js
+++ vlad/goToScripts.js
@@ -3,3 +3,14 @@ function GoToUsers()
 	var div = document.getElementById("V_Block1");
 	document.location.href = "users.php"
 }
+
+
+	 function PopUp(){
+        var popup = document.getElementById("popup1");
+        popup.classList.add('show');
+    }
+
+    function ClosePopup(){
+        var popup = document.getElementById("popup1");
+        popup.classList.remove('show');
+    }
--- vlad/vlad.php
+++ vlad/vlad.php
@@ -7,18 +7,22 @@
 	<script src="goToScripts.js"></script>
 </head>
 <body>
-<div align="center" id="V_BlockParent" >
-	<div id="V_DivHeader">
-		
-	<p ><font size=72px>Привет, это сайт Влада</font></p>
+
+	<!--POPUP-->
+	<div class="popup" id="popup1">ПРИВЕТ!!!
+		<div id="ButtonClosePopup" onclick="ClosePopup()">Close</div>
 	</div>
+	<div align="center" id="V_BlockParent" >
+		<div id="V_DivHeader">
+		<p ><font>Привет, это сайт Влада</font></p>
+		</div>
 	</div>
 	<div align="center" id="V_BlockParent" >
 		<div  onclick="GoToUsers()" class="V_BattonBlock" id="V_Block1">
 			Пользователи
 		</div>
 
-		<div class="V_BattonBlock" id="V_Block2">
+		<div class="V_BattonBlock" id="V_Block2" onclick="PopUp()">
 			2 Block
 		</div>
 		<div class="V_BattonBlock" id="V_Block3">
